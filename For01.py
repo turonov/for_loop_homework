4dc002e371557bf44e5c62e9c91ef2f3f6c9ebf0
turonov <turonov5400gmail.com>
2022-11-01 10:44:00 +0500

    01

--- For01.py
+++ For01.py
@@ -9,4 +9,11 @@ def main(n):
     Returns:
         list: return  answer
     """
-    return
+    list=[]
+    a=0
+    while a<n:
+        list=list+[a]
+        a+=1
+    return list
+print(main(6))
+
